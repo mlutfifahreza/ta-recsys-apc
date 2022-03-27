b0e4f7aade641231336651f3c5d01a54d413ec21
mlutfifahreza <mlutfifahreza@gmail.com>
2022-03-27 18:08:20 +0700

    refactor tracks random validation

--- data-get_tracks_random.py
+++ data-get_tracks_random.py
@@ -1,5 +1,6 @@
-import os,tqdm, csv, random
+import os, csv
 import numpy as np
+from tqdm import tqdm
 
 # Constants
 TRACKS_COUNT = 2262292
@@ -16,9 +17,9 @@ random_data_result = []
 row_number = 0
 with open(read_path_name) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
+    for row in tqdm(csv_reader):
         row_number += 1
         if row_number in row_number_to_get:
-            # random_data_result.append([row_number, *row])
-            print(row_number)
-            print(row)
+            random_data_result.append([row_number, *row])
+for data in random_data_result:
+    print(data)
